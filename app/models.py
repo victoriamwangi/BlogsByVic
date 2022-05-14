4ebb9212ad31dd072e6f5796c8d800d316775d9b
victoria mwangi <victoriamwangi021@gmail.com>
2022-05-14 13:41:29 +0300

    feat: initialize app

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from . import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+    
---
+++ manage.py
@@ -0,0 +1,16 @@
+from app import create_app
+from flask_script import Manager, Server
+
+
+app = create_app('production')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app= app)
+
+
+if __name__ == '__main__':
+    manager.run()
