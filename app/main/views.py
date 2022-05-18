432282d3d3250794beafe89340018e7b9bbbd1b8
victoria mwangi <victoriamwangi021@gmail.com>
2022-05-19 01:49:10 +0300

    st: edit errors views

--- app/main/views.py
+++ app/main/views.py
@@ -13,7 +13,6 @@ from ..request import get_Quotes
 @main.route('/')
 def index():
     blogs = Blog.query.order_by(Blog.posted.desc()).all()   
-    # blogQuote= get_blogQuotes('')
     quotes = get_Quotes()
     return render_template('index.html', blogs=blogs, quotes= quotes ) 
 
@@ -24,18 +23,17 @@ def profile(uname):
     blogs = Blog.query.filter_by(user_id = current_user.id).all()
     
     if user is None:
-        abort(404)
-
+        
+        return render_template('errors/404.html')
     return render_template("profile/profile.html", user=uname, blogs= blogs)
 
 @main.route('/user/<uname>/blogs/new', methods = ['GET', 'POST'])
 @login_required
 def new_blog(uname):
     user = User.query.filter_by(username=uname).first()
-    
     if user is None:
-        abort(404)
-  
+        return render_template('errors/404.html')
+
     form = BlogForm()
 
     if form.validate_on_submit():      
@@ -70,7 +68,8 @@ def delete_blog(id):
 def delete_comment(comment_id):
     comment =Comment.query.get_or_404(comment_id)
     if (comment.commenter) != current_user.id:
-        abort(403)
+        return render_template('errors/403.html')
+
     db.session.delete(comment)
     db.session.commit()
     flash('The comment has been deleted!')
@@ -99,14 +98,15 @@ def view_blog(id):
 def update(id):
     blog = Blog.query.get_or_404(id)
     if blog.user_id != current_user.id:
-        abort(403)
+        return render_template('errors/403.html')
+
     form = BlogForm()
     if form.validate_on_submit():
         blog.blog_title = form.blog_title.data
         blog.blog_content = form.blog_content.data
         db.session.commit()
 
-        return redirect(url_for('main.index'))#h
+        return redirect(url_for('main.index'))
     elif request.method == 'GET':
         form.blog_title.data = blog.blog_title
         form.blog_content.data = blog.blog_content
